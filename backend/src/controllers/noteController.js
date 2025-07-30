bc4851180520cd5e356a4b462b610995c7550766
Nirmitha Heshara <nirmithaheshara527@gmail.com>
2025-07-31 01:32:27 +0530

    create controllers for GET and POST request

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -1,9 +1,27 @@
-export function getAllNotes (req, res)  {
-    res.status(200).send("Get all notes");
+import Note from '../models/Note.js';
+
+export async function getAllNotes (req, res)  {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getAllNotes controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+    
 }
 
-export function createNote(req, res) {
-    res.status(201).json({ message: "Note created" });
+export async function createNote(req, res) {
+    try {
+        const { title, content } = req.body;
+        const newNote = new Note({title, content});
+
+        await newNote.save();
+        res.status(201).json({message: "Note created successfully"});
+    } catch (error) {
+        console.error("Error in createNote controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
 export function updateNote(req, res)  {
